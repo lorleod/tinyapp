16e8f28603e1502c9560ecd29ce5e6d5a92540b7
lorleod <taylor.mcleod@gmail.com>
2022-05-16 23:41:44 +0000

    added ejs engine

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const PORT = 8080; // default port 8080
+
+app.set("view engine", "ejs")
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -14,6 +17,10 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase)
 })
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>world</b></body></html>\n");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
