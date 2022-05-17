e5c017a1f9825151c9bad4a8e2f7ba2ffdc42475
lorleod <taylor.mcleod@gmail.com>
2022-05-17 05:52:13 +0000

    form work

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
+const bodyParser = require('body-parser');
 const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -19,9 +21,20 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
 
 app.get("/urls/:shortURL", (req, res) => {
   console.log(req.params.shortURL)
