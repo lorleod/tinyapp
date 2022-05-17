d5b4b6caeeb87f75d76f0a8952f55c8a5bc35ee0
lorleod <taylor.mcleod@gmail.com>
2022-05-17 01:46:31 +0000

    made individual shortened url page

--- express_server.js
+++ express_server.js
@@ -3,11 +3,11 @@ const res = require("express/lib/response");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
@@ -15,13 +15,23 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase};
+  const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase)
-})
+  res.json(urlDatabase);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  console.log(req.params.shortURL)
+  const shortURL = req.params.shortURL;
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[shortURL],
+  };
+  res.render("urls_show", templateVars);
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>world</b></body></html>\n");
