942f33a087ca447f1f3bf5d1426f2f85b9eb6bda
lorleod <taylor.mcleod@gmail.com>
2022-05-18 23:48:24 +0000

    create registration handler

--- express_server.js
+++ express_server.js
@@ -1,5 +1,5 @@
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 const express = require("express");
 const res = require("express/lib/response");
 const app = express();
@@ -14,10 +14,23 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+}
+
 app.get("/", (req, res) => {
   const templateVars = {
-  //  username: req.cookies["username"]
-  }
+    //  username: req.cookies["username"]
+  };
 
   res.render("/urls", templateVars);
 });
@@ -31,7 +44,7 @@ app.get("/urls", (req, res) => {
 app.get("/u/:shortURL", (req, res) => {
   console.log("URL database", urlDatabase);
   //console.log("REQ.params.shortURL: " + req.params.shortURL);
-  const shortURL = req.params.shortURL
+  const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
   console.log(longURL);
   res.redirect(longURL);
@@ -39,8 +52,8 @@ app.get("/u/:shortURL", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"]
-  }
+    username: req.cookies["username"],
+  };
 
   res.render("urls_new", templateVars);
 });
@@ -51,21 +64,44 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: shortURL,
     longURL: urlDatabase[shortURL],
-    username: req.cookies["username"]
+    username: req.cookies["username"],
   };
   res.render("urls_show", templateVars);
+});
+
+app.get("/register", (req, res) => {
+  const templateVars = {
+    username: req.cookies["user_id"],
+  };
+  res.render('register', templateVars);
+});
+
+app.post("/register", (req, res) => {
+
+  const newUserID = generateRandomString();
+  const email = req.body.email;
+  const password = req.body.password;
+
+  users[newUserID] = {
+    id: newUserID,
+    email: email,
+    password: password
+  }
+  console.log(users);
+  res.cookie("user_id", newUserID);
+  res.redirect("/urls");
 });
 
 app.post("/login", (req, res) => {
   // long URL from form
   const username = req.body.username;
-  res.cookie('username', username);
-  res.redirect('/urls');
+  res.cookie("username", username);
+  res.redirect("/urls");
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
-  res.redirect('/urls');
+  res.clearCookie("user_id");
+  res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL", (req, res) => {
@@ -75,7 +111,7 @@ app.post("/urls/:shortURL", (req, res) => {
   const longURL = req.body.longURL;
 
   urlDatabase[shortURL] = longURL;
-  res.redirect('/urls');
+  res.redirect("/urls");
 });
 
 app.post("/urls", (req, res) => {
@@ -96,7 +132,7 @@ app.post("/urls", (req, res) => {
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
-  res.redirect('/urls');
+  res.redirect("/urls");
 });
 
 // app.get("/urls.json", (req, res) => {
